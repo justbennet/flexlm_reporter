61d3bde14f45efb0c9ea4a0ffc5b08ecc5ad2e32
Bennet Fauber <bennet@umich.edu>
2017-07-04 15:17:35 -0400

    Modified test to use multiple features and random data

--- graphite/test.py
+++ graphite/test.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import datetime
+import random
 
 # This was the old location
 # hpc/software/licenses/FEATURE
@@ -16,32 +17,40 @@ carbon_port = 2003
 timestamp = datetime.datetime.now().strftime('%s')
 
 # create data to test
-daemon  = 'hello'
-feature = 'greeting'
-used   = '4'
-total   = '10'
-users   = '2'
+daemon   = 'greeting'
+features = [ 'hello', 'yowza', 'whazzup' ]
 
 # initialize empty message list
 messages = []
 
-messages.append(' '.join(['hpc.flux.software.licenses' + '.'
-                           + daemon + feature + '.'
-                           + 'number_available', total, timestamp]))
-messages.append(' '.join(['hpc.flux.software.licenses' + '.'
-                           + daemon + feature + '.'
-                           + 'number_used', used, timestamp]))
-messages.append(' '.join(['hpc.flux.software.licenses' + '.'
-                           + daemon + feature + '.'
-                           + 'number_users', users, timestamp]))
+for feature in features:
+    # generate some fake data
+    available   = '50'
+    used        = str(random.randrange(2, int(available)))
+    users       = str(random.randrange(1, int(used)))
 
+    # line it up for sending
+    feature_root = '.'.join(['hpc.flux.software.licenses', daemon, feature])
+    messages.append(' '.join([feature_root + '.available',
+                             available, timestamp]))
+    messages.append(' '.join([feature_root + '.users',
+                             users, timestamp]))
+    messages.append(' '.join([feature_root + '.used',
+                             used, timestamp]))
+messages.append('')
+
+# set up the socket and timeout
 sock = socket.socket()
 sock.settimeout(10.0)
 
 try:
+    # message away
     sock.connect((carbon_url, carbon_port))
     sock.sendall('\n'.join(messages).encode('utf-8'))
     sock.close()
+    # we want confirmation for now
+    for line in messages:
+        print(line)
 except:
     print('Problem sending data to Graphite/Carbon')
     sys.exit(1)
